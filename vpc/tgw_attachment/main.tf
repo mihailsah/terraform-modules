57c8741d05fb227219ef1eb68786e4d620c90374
Mihail Sahatchiev <mihail_sahatchiev@flutterint.com>
2022-06-15 14:19:03 +0300

    dsa

--- vpc/tgw_attachment/main.tf
+++ vpc/tgw_attachment/main.tf
@@ -2,7 +2,7 @@
 # TGW Attachment
 #############################
 
-resource "aws_transit_gateway_vpc_attachment" "tgw_attach" {
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attach" {
   subnet_ids         = var.subnet_ids
   transit_gateway_id = var.transit_gateway_id
   vpc_id             = var.vpc_id
