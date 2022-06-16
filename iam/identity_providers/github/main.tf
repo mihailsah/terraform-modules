2bea86d36a5ab6d86703ec194e955fb67607509e
Mihail Sahatchiev <mihail_sahatchiev@flutterint.com>
2022-06-16 17:36:28 +0300

    left over code

--- iam/identity_providers/github/main.tf
+++ iam/identity_providers/github/main.tf
@@ -1,31 +1,6 @@
 ####################################
-# Locals
-####################################
-
-locals {
-  service_name = "${var.platform}-${var.project}-${local.environment}"
-  tags = {
-    platform    = var.platform
-    project     = var.project
-    environment = local.environment
-    region      = var.region
-    terraform   = "true"    
-    code_repo   = var.code_repo
-  }
-  environment = terraform.workspace
-}
-
-####################################
 # Resources
 ####################################
-
-module "dev_deploy_role" {
-  source = "./modules/role"
-  name = "gh-oidc-dev-deploy-role"
-  subject_claims = var.repo
-  oidc_provider_arn = data.aws_iam_openid_connect_provider.github.arn
-  policy = file("./policies/s3_role_permissions_policy.json")
-}
 
 # IAM
 resource "aws_iam_openid_connect_provider" "github_actions" {
