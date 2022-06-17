8c7cbcdd242edc0655bfc4fd30279a60cf8b1851
Mihail Sahatchiev <mihail_sahatchiev@flutterint.com>
2022-06-17 14:58:06 +0300

    cng

--- iam/identity_providers/github/variables.tf
+++ iam/identity_providers/github/variables.tf
@@ -3,3 +3,19 @@ variable "thumbprint_list" {
   type        = list(string)
   default     = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
+
+variable "name" {
+  type = string
+}
+
+variable "subject_claims" {
+  type = list(string)
+}
+
+variable "oidc_provider_arn" {
+  type = string
+}
+
+variable "policy" {
+  type = string
+}
